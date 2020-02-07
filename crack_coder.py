4df0b724ffb2e42e0fca5676eb49bf794a171e9d
LittleKitacho <LittleKitacho@outlook.com>
2020-02-07 16:57:17 -0500

    Add encode function & direct running method

--- crack_coder.py
+++ crack_coder.py
@@ -1,5 +1,94 @@
-charlist = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890`~!@#$%^&*()-_=+[{]}"
-len_id = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
+from random import randint, choice
+
+letters = "abcdefghijklmnopqrstuvwxyz"
+capitals = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numbers = "1234567890"
+symbols = "symbols"
+
+length_id = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ"
 
 def encode (decoded_str: str, length: int) -> str:
+
+    if length > len(length_id):
+        length = len(length_id) - 1
+    
+    else:
+        length -= 1
+
+    has_letters = False
+    has_capitals = False
+    has_numbers = False
+    has_symbols = False
+
+    for i in decoded_str:
+        if i in letters:
+            has_letters = True
+
+        elif i in capitals:
+            has_capitals = True
+
+        elif i in numbers:
+            has_numbers = True
+
+        elif i in symbols:
+            has_symbols = True
+
+    possible_chars = ""
+
+    if has_letters:
+        possible_chars += letters
+
+    if has_capitals:
+        possible_chars += capitals
+    
+    if has_numbers:
+        possible_chars += numbers
+    
+    if has_symbols:
+        possible_chars += symbols
+    
+    encoded_str = decoded_str[0]
+    x = 1
+
+    while x < len(decoded_str):
+
+        y = 0
+
+        while y <= length:
+
+            encoded_str += choice(possible_chars)
+            y += 1
+        
+        encoded_str += decoded_str[x]
+        x += 1
+
+    return encoded_str + length_id[length]
+            
+
+
+def decode (encoded_str: str) -> str:
     pass
+
+# # # # # # # # # #
+
+if __name__ == "__main__":
+
+    a = input('Enter \'encode\' to encode a string, and \'decode\' to decode an encoded string.  ')
+    b = None
+
+    while b == None:
+
+        if a == 'encode':
+
+            b = input('Please input a string to encode.  ')
+            
+            print('Your encoded string is \'' + encode(b) + '"'")
+
+        elif a == 'decode':
+
+            b = input('Please input an encoded string to decode.  ')
+            
+            print('Your decoded string is \'' + encode(b) + "'")
+
+        else:
+            a = input('Sorry, please either input \'encode\' or \'decode\'.  ') 
